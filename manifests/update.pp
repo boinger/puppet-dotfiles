86d1077305b9591c6b51eafd69ad3bb285e89d77
jeff@jeffvier.com <jeff@jeffvier.com>
2013-04-25 16:55:32 -0700

    FETCH_HEAD does not exist until a successful 'git pull'

--- manifests/update.pp
+++ manifests/update.pp
@@ -11,7 +11,7 @@ define dotfiles::update(
     exec { "update ${gituser} dotfiles for ${title}":
       cwd     => "${cwd}",
       command => $rebase ? {true  => "git pull --rebase", false => "git pull", },
-      onlyif  => "[ \"$(( ( $(date +%s) - $(stat -c \"%Y\" .git/FETCH_HEAD) ) / 60 ))\" -ge \"${frequency}\" ]",
+      onlyif  => "[ ! -f .git/FETCH_HEAD ] || [ \"$(( ( $(date +%s) - $(stat -c \"%Y\" .git/FETCH_HEAD) ) / 60 ))\" -ge \"${frequency}\" ]",
       user    => "${title}",
       require => Package['git'],
     }
